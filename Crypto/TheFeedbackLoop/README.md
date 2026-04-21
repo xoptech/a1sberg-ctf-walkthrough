ebd1679faa27bb0efe28bdbf040eddd047b07567
IMO <180146096+IMOitself@users.noreply.github.com>
2026-04-22 00:38:52 +0800

    Crypto\TheFeedbackLoop: challenge complete

---
+++ Crypto/TheFeedbackLoop/README.md
@@ -0,0 +1,7 @@
+1. get the first 32 bits on keystream leak on message.txt
+2. reverse it
+3. seperate it into 8 bits (result in 4 group)
+4. in cyberchef use from binary recipe
+5. then use to hex recipe. combine all the hex.
+6. use it as the SEED on the py file
+7. XOR shenanigans idk idk (WHY DO THEY PUT BAD VARIABLE NAMES)
--- Crypto/TheFeedbackLoop/encrypt.py
+++ Crypto/TheFeedbackLoop/encrypt.py
@@ -1,8 +1,10 @@
 import os
 
 FLAG = b"REDACTED"
-SEED = int.from_bytes(FLAG[:4], 'big')
+SEED = 0x4131537b
 MASK = 0b10000000000000000000000000000111
+ciphertext_hex = "3a62623a64ab321c4edd43f73b8228c971207c5366de57feba1465412a3344beb2"
+ciphertext = bytes.fromhex(ciphertext_hex)
 
 class LFSR:
     def __init__(self, seed, mask):
@@ -35,5 +37,16 @@ def encrypt():
     
     print(f"Ciphertext: {bytes(ciphertext).hex()}")
 
+def decrypt():
+    lfsr_dec = LFSR(SEED, MASK)
+    flag = []
+    for byte in ciphertext:
+        keystream_byte = 0
+        for i in range(8):
+            keystream_byte |= (lfsr_dec.next_bit() << i)
+        flag.append(byte ^ keystream_byte)
+    
+    print(f"flag: {bytes(flag).decode()}")
+
 if __name__ == "__main__":
-    encrypt()
+    decrypt()
