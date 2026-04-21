ff5604ff7d1ee7838084a966fb8066ff136934bc
IMO <180146096+IMOitself@users.noreply.github.com>
2026-04-22 04:45:05 +0800

    repo: a way to screenshot the progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # a1sberg-ctf-walkthrough
 answering challenges in the practice website provided for the upcoming kyokugen ctf 2026
+
+![](screenshot.png)
---
+++ screenshot-code.js
@@ -0,0 +1,9 @@
+let s = document.createElement('script');
+s.src = 'https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.11.11/html-to-image.min.js';
+document.head.appendChild(s);
+s.onload = () => htmlToImage.toPng(document.querySelector('.main-content')).then(d => {
+    let a = document.createElement('a');
+    a.download = 'exact-screenshot.png';
+    a.href = d;
+    a.click();
+});
