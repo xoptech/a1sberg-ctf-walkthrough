ef794a9d6f54fb963ddcb62788c0859e977bf652
IMO <180146096+IMOitself@users.noreply.github.com>
2026-04-24 01:33:25 +0800

    repo: update screenshot

--- screenshot-code.js
+++ screenshot-code.js
@@ -1,3 +1,4 @@
+// use this on browser's console when on the challenges page
 let s = document.createElement('script');
 s.src = 'https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.11.11/html-to-image.min.js';
 document.head.appendChild(s);
